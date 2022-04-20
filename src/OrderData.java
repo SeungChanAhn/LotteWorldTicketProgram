7f2998b2b2e970d480d504473be06e19f6e71145
SeungChanAhn <asc7230@gmail.com>
2022-04-20 18:30:12 +0900

    수정중..

---
+++ src/OrderData.java
@@ -0,0 +1,19 @@
+package Practice;
+
+public class OrderData {
+	public int ticketChoice;
+	public int timeChoice;
+	public int price;
+	public int priceSum;
+	public int totalPrice;
+	public int WhetherToBuy;
+	public String IDNumber;
+	public int numberOfTickets;
+	public int alwaysPrefer;
+	public int today, year, month, day;
+	public int IDNumber_Front;
+	public int gender;
+	public int age, realAge;
+	public int personTypeByAge;
+	public int kidsPlayroom;
+}
---
+++ src/Receipt.java
@@ -0,0 +1,109 @@
+package Practice;
+
+import java.util.ArrayList;
+
+public class Receipt {	
+	Calculation cal;
+	Receipt(Calculation cal) {
+		this.cal = cal;
+	}
+
+	ArrayList orderList = new ArrayList();
+
+	int totalPrice = 0;
+	
+	public void changeST() {		
+//		data = new OrderData();
+		
+		String ticket = null;
+		String time = null;
+		String ageCategory = null;
+		String ticketCount = null;
+		String prefer = null;
+
+		switch (cal.input.data.ticketChoice) {
+		case 1:
+			ticket = "종합이용권";
+			break;
+		case 2:
+			ticket = "파크이용권";
+			break;
+		}
+
+		switch (cal.input.data.timeChoice) {
+		case 1:
+			time = "종일권";
+			break;
+		case 2:
+			time = "After4";
+			break;
+		}
+		
+		switch (cal.input.data.personTypeByAge) {
+		case 0:
+			ageCategory = "노인";
+			break;
+		case 1:
+			ageCategory = "어른";
+			break;
+		case 2:
+			ageCategory = "청소년";
+			break;
+		case 3:
+			ageCategory = "어린이";
+			break;
+		case 4:
+			ageCategory = "베이비";
+			break;
+		}
+		
+		switch (cal.input.data.alwaysPrefer) {
+		case 0:
+			prefer = "경로 우대";
+			break;
+		case 1:
+			prefer = "장애인 우대";
+			break;
+		case 2:
+			prefer = "국가유공자 우대";
+			break;
+		case 3:
+			prefer = "휴가군인 우대";
+			break;
+		case 4:
+			prefer = "임산부 우대";
+			break;
+		case 5:
+			prefer = "다둥이가족 우대";
+			break;
+		case 6:
+			prefer = "해당사항 없음";
+			break;
+		}
+
+		String all = ticket + "\t\t" + time + "\t\t\t" + ageCategory + "\t\t" + cal.input.data.numberOfTickets + "장" + "\t\t" + cal.input.data.priceSum + "원" + "\t\t" + prefer + "\n";
+		orderList.add(all);
+		
+
+	}
+
+	public void printReceipt() {
+		System.out.printf(
+				"=============================================================================================================\n");
+		System.out.printf("\n\n                                                롯 데 월 드\n");
+		System.out.printf("\n                                                     \n");
+		System.out.printf(
+				"=============================================================================================================\n");
+		System.out.printf("티켓종류\t\t이용시간\t\t구분\t\t수량\t\t가격\t\t할인내역\n");
+		System.out.printf(
+				"-------------------------------------------------------------------------------------------------------------\n");
+		for (int i = 0; i < orderList.size(); i++) {
+			System.out.println(orderList.get(i));
+		}
+		System.out.printf(
+				"\n-------------------------------------------------------------------------------------------------------------\n");
+		System.out.printf("결제금액\t\t\t\t\t\t\t\t\t\t%19d원", cal.input.data.totalPrice);
+		System.out.printf(
+				"\n=============================================================================================================\n");
+	}
+}
